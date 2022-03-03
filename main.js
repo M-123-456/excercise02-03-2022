d2b6847201e758e03ba98f716f07d789cd2c3a6e
M-123-456 <mikigerlach2@gmail.com>
2022-03-03 14:53:01 +0100

    solution checked

--- main.js
+++ main.js
@@ -1,5 +1,9 @@
 import { test } from "./test.js";
+import { addParams } from "./test.js";
 console.log(test);
+console.log(addParams("nnn"));
+
+// ##############################################################################
 
 import { customers } from "./customers.js";
 import { orders } from "./orders.js";
@@ -9,36 +13,37 @@ console.log(
   `There are ${customers.length} customers, ${orders.length} orders and ${products.length} products`
 );
 
-console.log();
-console.log("====Q1====");
-console.log();
+const title = (title) => {
+  console.log(`
+
+  === ${title} ===
+  
+  `);
+};
+
 // (1) display all company names of customers
+title("(1) display all company names of customers");
 customers.forEach((m) => console.log(m.companyName));
 
-console.log();
-console.log("====Q2====");
-console.log();
 // (2) display **city - customerName**
+title("(2) display **city - customerName**");
 customers.forEach((m) => console.log(`${m.address.city} - ${m.companyName}`));
 
-console.log();
-console.log("====Q3====");
-console.log();
 // (3) display **city - customerName** only for those from London
-const customersInUK = customers.filter((m) => m.address.country === "UK");
-customersInUK.forEach((m) =>
+title("(3) display **city - customerName** only for those from London");
+const customersInLondon = customers.filter((m) => m.address.city === "London");
+customersInLondon.forEach((m) =>
   console.log(`${m.address.city} - ${m.companyName}`)
 );
 
-console.log();
-console.log("====Q4====");
-console.log();
 //(4) create function `displayCustomerOrders(customerId)` which returns a string the following information
 /*
 - `console.log(displayCustomerOrders('AROUT'));`
 - returns Around the Horn (AROUT) has 14 orders
 */
-
+title(
+  "(4) create function `displayCustomerOrders(customerId)` which returns a string the following information"
+);
 const displayCustomerOrders = (customerId) => {
   const customer = customers.find((m) => m.customerID === customerId);
   const custOrders = orders.filter((m) => m.customerID === customerId);
@@ -47,9 +52,29 @@ const displayCustomerOrders = (customerId) => {
 
 console.log(displayCustomerOrders("AROUT"));
 
-console.log();
-console.log("====Q5====");
-console.log();
+const displayCustomerOrdersAndProductKinds = (customerId) => {
+  const numOfProducts = orders
+    .filter((m) => m.customerID === customerId)
+    .reduce((sum, order) => {
+      return sum + order.details.length;
+    }, 0);
+
+  return `${numOfProducts} products`;
+};
+
+console.log(displayCustomerOrdersAndProductKinds("AROUT"));
+
+const displayCustomerOrdersAndProducts = (customerId) => {
+  const numOfProducts = orders
+    .filter((m) => m.customerID === customerId)
+    .reduce((sum, order) => {
+      sum += order.details.length;
+      return sum;
+    }, 0);
+};
+
+console.log(displayCustomerOrdersAndProducts("AROUT"));
+
 // (5) create function `getCustomerOrderObject(customerId)` which returns an objects the following information:
 /*
   - `console.log(getCustomerOrderObject('AROUT'));`
@@ -63,20 +88,21 @@ console.log();
 }
 ```
 */
+title(
+  "(5) create function `getCustomerOrderObject(customerId)` which returns an objects the following information:"
+);
+
 const getCustomerOrderObject = (customerId) => {
   const customer = customers.find((m) => m.customerID === customerId);
   const custOrders = orders.filter((m) => m.customerID === customerId);
   return {
-    id: customer.customerID,
+    id: customerId,
     name: customer.companyName,
     orderIds: custOrders.map((m) => m.orderID),
   };
 };
 console.log(getCustomerOrderObject("AROUT"));
 
-console.log();
-console.log("====Q6====");
-console.log();
 // (6) create function `getProductsOrderedByCustomer(customerId)` which returns an array of the names of products a customer has ordered:
 /*
 [
@@ -87,32 +113,38 @@ console.log();
 ]
 */
 
+title(
+  "(6) create function `getProductsOrderedByCustomer(customerId)` which returns an array of the names of products a customer has ordered:"
+);
+
 // function getProductsById to get product name by product ID
-const getProductsById = (productId) => {
+const getProductById = (productId) => {
   const productName = products.find((m) => m.productID === productId).name;
   return productName;
 };
 
 // function getProductsOrderedByCustomer to get name of products ordered by a customer
 const getProductsOrderedByCustomer = (customerId) => {
-  // get orders from the customer
+  // find customer
+  const customer = customers.find((m) => m.customerID === customerId);
+  // filter orders by customer
   const custOrders = orders.filter((m) => m.customerID === customerId);
-
   // get product IDs of ordered products by the customer
-  // orders.map ->
   const productIds = custOrders
-    .map((m) => m.details.map((n) => n.productID))
+    .map((order) => order.details.map((detail) => detail.productID))
     .flat();
 
   // get array with names of products listed up in productIds
-  const productNames = [];
-  // go through the productIds
+  const orderedProducts = [];
   productIds.forEach((id) => {
-    // push the product name got by getProductsById to array productNames
-    productNames.push(getProductsById(id));
+    orderedProducts.push(getProductById(id));
   });
 
-  return productNames;
+  return {
+    id: customerId,
+    name: customer.companyName,
+    orderedProducts,
+  };
 };
 
 console.log(getProductsOrderedByCustomer("AROUT"));
